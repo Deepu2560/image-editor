b86a2f8a7c0b28a23dcb0242227f820e99936d33
unknown <deepanshugulia256@gmail.com>
2023-04-18 22:20:00 +0530

    adding javascript functionality

---
+++ index.js
@@ -0,0 +1,66 @@
+const fileInput = document.querySelector(".flie-input");
+const chooseFIle = document.querySelector(".choose-img");
+const previewImg = document.querySelector(".preview-img img");
+const filterOptions = document.querySelectorAll(".filter button");
+const filterName = document.querySelector(".filter-info .name");
+const filterValue = document.querySelector(".filter-info .value");
+const filterSlider = document.querySelector(".slider input");
+
+let brightness = 100;
+let inversion = 0;
+let saturation = 100;
+let grayscale = 0;
+
+const loadImage = () => {
+  let file = fileInput.files[0];
+  if (!file) return;
+  previewImg.src = URL.createObjectURL(file);
+  previewImg.addEventListener("load", () => {
+    document.querySelector(".container").classList.remove("disable");
+  });
+};
+
+const updatefilter = () => {
+  filterValue.innerText = filterSlider.value + "%";
+  const selectedFilter = document.querySelector(".filter .active");
+
+  if (selectedFilter.id === "brightness") {
+    brightness = filterSlider.value;
+  } else if (selectedFilter.id === "inversion") {
+    inversion = filterSlider.value;
+  } else if (selectedFilter.id === "grayscale") {
+    grayscale = filterSlider.value;
+  } else if (selectedFilter.id === "saturation") {
+    saturation = filterSlider.value;
+  }
+};
+
+filterOptions.forEach((option) => {
+  option.addEventListener("click", () => {
+    document.querySelector(".filter .active").classList.remove("active");
+    option.classList.add("active");
+    filterName.innerText = option.innerText;
+
+    if (option.id === "brightness") {
+      filterSlider.max = "200";
+      filterSlider.value = brightness;
+      filterValue.innerText = `${brightness}%`;
+    } else if (option.id === "inversion") {
+      filterSlider.max = "100";
+      filterSlider.value = inversion;
+      filterValue.innerText = `${inversion}%`;
+    } else if (option.id === "grayscale") {
+      filterSlider.max = "100";
+      filterSlider.value = grayscale;
+      filterValue.innerText = `${grayscale}%`;
+    } else if (option.id === "saturation") {
+      filterSlider.max = "200";
+      filterSlider.value = saturation;
+      filterValue.innerText = `${saturation}%`;
+    }
+  });
+});
+
+filterSlider.addEventListener("input", updatefilter);
+fileInput.addEventListener("change", loadImage);
+chooseFIle.addEventListener("click", () => fileInput.click());
